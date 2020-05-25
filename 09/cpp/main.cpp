eebc765db9fd80c858dea3fc265bf98c2b74c0a9
Andrea Rossoni <andrea.ros21@mail.com>
2020-05-25 09:32:26 +0200

    Complete Ex09

--- 09/cpp/main.cpp
+++ 09/cpp/main.cpp
@@ -14,38 +14,29 @@
 int
 main(int argc, char * argv[])
 {
-    unsigned int nIter(10);
-
-    // randomOnCircle(32);
-    // randomInSquare(32);
-
-    Population popOC("circle", 50);
-    Population popIS("square", 50);
-
     /***************************
     *  Exercise 09.1.1
     ***************************/
 
-    GeneticAlgo GAOC(0.1, 0.5);
-
-    vector<double> bestL1OC(nIter);
-    vector<double> bestHalfAvgL1OC(nIter, 0.0d);
-
-    for (unsigned int i = 0; i < nIter; ++i) {
-        popOC       = GAOC.evolvePopulation(popOC);
-        bestL1OC[i] = popOC.getTour(0).getDistance();
-        // for (unsigned int j = 0; j < (unsigned int) (0.5 * popOC.getPopulationSize()); ++j)
-        //     bestHalfAvgL1OC[i] += popOC.getTour(j).getDistance();
-        // bestHalfAvgL1OC[i] /= (0.5 * popOC.getPopulationSize());
-    }
-
-    popOC.getTour(0).writeTourToFile("OC");
-    writeVector(bestL1OC, "bL1OC");
-    // writeVector(bestHalfAvgL1OC, "bHAL1OC");
+    // randomOnCircle(32);
+    //
+    // Population popOC("circle", 50);
+    //
+    // GeneticAlgo GAOC(0.5, 0.4);
+    //
+    // GAOC.evolvePopulation(popOC, "OC", 300, true, true, true);
 
     /***************************
     *  Exercise 09.1.2
     ***************************/
+
+    // randomInSquare(32);
+
+    Population popIS("square", 50);
+
+    GeneticAlgo GAIS(0.8, 0.5);
+
+    GAIS.evolvePopulation(popIS, 700, "IS", true, true, true);
 
     return 0;
 } // main
